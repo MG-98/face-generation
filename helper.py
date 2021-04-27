8948e735de1b37d21869c2f1e2b676cea1df47ab
MG-98 <s-mahmoud_gamal@zewailcity.edu.eg>
2021-04-27 23:32:12 +0200

    face generation

---
+++ helper.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+# helper conv function
+def conv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm=True):
+    """Creates a convolutional layer, with optional batch normalization.
+    """
+    layers = []
+    conv_layer = nn.Conv2d(in_channels, out_channels, 
+                           kernel_size, stride, padding, bias=False)
+    
+    # append conv layer
+    layers.append(conv_layer)
+
+    if batch_norm:
+        # append batchnorm layer
+        layers.append(nn.BatchNorm2d(out_channels))
+     
+    # using Sequential container
+    return nn.Sequential(*layers)
+
+
+# helper deconv function
+def deconv(in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm=True):
+    """Creates a transposed-convolutional layer, with optional batch normalization.
+    """
+    ## TODO: Complete this function
+    ## create a sequence of transpose + optional batch norm layers
+    layers = []
+    transposed_conv_layer = nn.ConvTranspose2d(in_channels, out_channels, 
+                           kernel_size, stride , padding, bias=False)
+    
+    # append conv layer
+    layers.append(transposed_conv_layer)
+
+    if batch_norm:
+        # append batchnorm layer
+        layers.append(nn.BatchNorm2d(out_channels))
+     
+    # using Sequential container
+    return nn.Sequential(*layers)
+        
